747a2f089224cd4d623aafa7b6038f941549e695
Daniel Coutinho <36049697+danmrc@users.noreply.github.com>
2022-04-27 16:17:46 -0300

    Krussel Smith test that works

--- krusell_smith.py
+++ krusell_smith.py
@@ -1,3 +1,5 @@
+# Can only run on a notebook, decorators do not work on emacs?!
+
 import copy
 import numpy as np
 import matplotlib.pyplot as plt
@@ -10,19 +12,20 @@ def household_guess(a_grid,e_grid,r,w,gamma):
     V_prime = (1+r)*(wel/2)**(-gamma)
     return V_prime
 
+# Important: V_prime MUST be named v_prime_p in the argument of the function! Otherwise, raises error
 
-@het(exogenous = 'Pi',policy = 'a', backward = 'V_prime_new', backward_init=household_guess)
-def household(V_prime,a_grid,e_grid,r,w,beta,gamma,init = household_guess):
-    c_prime = (beta*V_prime)**(-1/gamma)
+@het(exogenous = 'Pi',policy = 'a', backward = 'V_prime', backward_init=household_guess)
+def household(V_prime_p,a_grid,e_grid,r,w,beta,gamma):
+    c_prime = (beta*V_prime_p)**(-1/gamma)
     new_grid = c_prime + a_grid
     wel = (1+r)*a_grid[np.newaxis,:] + w*e_grid[:,np.newaxis]
 
     a = interpolate.interpolate_y(new_grid,wel,a_grid)
     misc.setmin(a,a_grid[0])
     c = wel - a
-    V_prime_new = (1+r)*c**(-gamma)
+    V_prime= (1+r)*c**(-gamma)
 
-    return V_prime_new,a,c
+    return V_prime,a,c
 
 print(household)
 print(f'Inputs: {household.inputs}')
@@ -57,7 +60,7 @@ def mkt_clearing(K, A, Y, C, delta):
 ks = create_model([household_ext, firm, mkt_clearing], name="Krusell-Smith")
 print(ks.inputs)
 
-calibration = {'eis': 1, 'delta': 0.025, 'alpha': 0.11, 'rho_e': 0.966, 'sd_e': 0.5, 'L': 1.0,
+calibration = {'gamma': 1, 'delta': 0.025, 'alpha': 0.11, 'rho_e': 0.966, 'sd_e': 0.5, 'L': 1.0,
                'nE': 7, 'nA': 500, 'amin': 0, 'amax': 200}
 unknowns_ss = {'beta': 0.98, 'Z': 0.85, 'K': 3.}
 targets_ss = {'r': 0.01, 'Y': 1., 'asset_mkt': 0.}
