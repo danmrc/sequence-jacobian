184e2780db1a7980d80f388f2df0693f6c05a0c1
Daniel Coutinho <36049697+danmrc@users.noreply.github.com>
2022-05-02 21:51:05 -0300

    debugging do hank de um ativo

--- bissection_vectorize.py
+++ bissection_vectorize.py
@@ -10,10 +10,10 @@ def bissection_onestep(f,a,b):
         new_b = b
         indxs_a = np.nonzero(mid_value*f(b) <= 0)
         indxs_b = np.nonzero(mid_value*f(a) <= 0)
-        if indxs_a[0].size != 0 and indxs_a[1].size != 0:
-            new_a[indxs_a[0],indxs_a[1]] = mid_point[indxs_a[0],indxs_a[1]]
-        if indxs_b[0].size != 0 and indxs_b[0].size != 0:
-            new_b[indxs_b[0],indxs_b[1]] = mid_point[indxs_b[0],indxs_b[1]]
+        if indxs_a[0].size != 0:
+            new_a[indxs_a] = mid_point[indxs_a]
+        if indxs_b[0].size != 0:
+            new_b[indxs_b] = mid_point[indxs_b]
         return new_a,new_b
 
 def vec_bissection(f,a,b,iter_max = 100,tol = 1E-11):
--- hank_one.py
+++ hank_one.py
@@ -17,26 +17,27 @@ def household_guess(a_grid,e_grid,r,w,gamma,T):
     return V_prime
 
 def consumption(c,we,rest,gamma,v,phi):
-    return c - we*(we/(-phi*c**gamma))**(1/v) - rest
+    return c - we*(we/(phi*c**gamma))**(1/v) - rest
 
 
 # Important: V_prime MUST be named v_prime_p in the argument of the function! Otherwise, raises error
 
 @het(exogenous = 'Pi',policy = 'a', backward = 'V_prime', backward_init=household_guess)
+@het(exogenous = 'Pi',policy = 'a', backward = 'V_prime', backward_init=household_guess)
 def household(V_prime_p,a_grid,e_grid,r,w,T,beta,gamma,v,phi):
 
-    we = w*e_grid[:,np.newaxis]
+    we = w*e_grid
 
     c_prime = (beta*V_prime_p)**(-1/gamma) #c_prime is quite a misnomer, since this is the new guess for c_t
-    n_prime = (we/(phi*c_prime**gamma))**(1/v)
+    n_prime = (we[:,np.newaxis]/(phi*c_prime**gamma))**(1/v)
 
-    new_grid = c_prime + a_grid[np.newaxis,:] - we - T[:,np.newaxis]
+    new_grid = c_prime + a_grid[np.newaxis,:] - we[:,np.newaxis] - T[:,np.newaxis]
     wel = (1+r)*a_grid
 
     c = interpolate.interpolate_y(new_grid,wel,c_prime)
     n = interpolate.interpolate_y(new_grid,wel,n_prime)
 
-    a = wel + we*n + T[:,np.newaxis] - c
+    a = wel + we[:,np.newaxis]*n + T[:,np.newaxis] - c
     V_prime= (1+r)*c**(-gamma)
 
     # checks for violations of the condition of minimal assets required and fixes it
@@ -46,14 +47,16 @@ def household(V_prime_p,a_grid,e_grid,r,w,T,beta,gamma,v,phi):
 
     if indexes_asset[0].size != 0 and indexes_asset[1].size !=0:
 
-        aa = np.zeros((indexes_asset[0].size,indexes_asset[1].size))
-        bb = c_prime[indexes_asset[0],indexes_asset[1]]
-
+        aa = np.zeros((indexes_asset[0].size)) + 1E-5
         rest = -a_grid[0] + wel[indexes_asset[1]] + T[indexes_asset[0]]
-        c[indexes_asset] = vec_bissection(lambda c : consumption(c,we[indexes_asset[0]],rest,gamma,v,phi),aa,bb)
-        V_prime[indexes_asset] = (1+r)*c**(gamma)
+        bb = c[indexes_asset] + 0.5
 
-    return V_prime,a,c
+        c[indexes_asset] = vec_bissection(lambda c : consumption(c,we[indexes_asset[0]],rest,gamma,v,phi),aa,bb)
+        n[indexes_asset] = (we[indexes_asset[0]]/(phi*c[indexes_asset]**gamma))**(1/v)
+        V_prime[indexes_asset] = (1+r)*(c[indexes_asset])**(-gamma)
+
+    return V_prime,a,c,n
+
 
 print(household)
 print(f'Inputs: {household.inputs}')
@@ -95,8 +98,8 @@ def firm(Y, w, Z, pi, mu, kappa):
 
 
 @simple
-def monetary(pi, rstar, phi):
-    r = (1 + rstar(-1) + phi * pi(-1)) / (1 + pi) - 1
+def monetary(pi, rstar, phi_pi):
+    r = (1 + rstar(-1) + phi_pi * pi(-1)) / (1 + pi) - 1
     return r
 
 
@@ -118,3 +121,27 @@ def mkt_clearing(A, NE, C, L, Y, B, pi, mu, kappa):
 def nkpc_ss(Z, mu):
     w = Z / mu
     return w
+
+blocks_ss = [hh_ext, firm, monetary, fiscal, mkt_clearing, nkpc_ss]
+
+hank_ss = create_model(blocks_ss, name="One-Asset HANK SS")
+
+print(hank_ss)
+print(f"Inputs: {hank_ss.inputs}")
+
+calibration = {'gamma': 0.5, 'v': 0.5, 'rho_e': 0.966, 'sd_e': 0.5, 'nE': 7,
+               'amin': 0.001, 'amax': 150, 'nA': 500, 'Y': 1.0, 'Z': 1.0, 'pi': 0.0,
+               'mu': 1.2, 'kappa': 0.1, 'rstar': 0.005, 'phi_pi': 1.5, 'B': 5.6}
+
+unknowns_ss = {'beta': 0.986, 'phi': 0.8}
+targets_ss = {'asset_mkt': 0, 'labor_mkt': 0}
+
+ss0 = hank_ss.solve_steady_state(calibration, unknowns_ss, targets_ss, solver="hybr")
+
+print(f"Asset market clearing: {ss0['asset_mkt']: 0.2e}")
+print(f"Labor market clearing: {ss0['labor_mkt']: 0.2e}")
+print(f"Goods market clearing (untargeted): {ss0['goods_mkt']: 0.2e}")
+
+plt.plot(ss0.internals['hh']['a_grid'], ss0.internals['hh']['n'].T)
+plt.xlabel('Assets'), plt.ylabel('Labor supply')
+plt.show()
